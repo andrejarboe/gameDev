4a78bef902e0635f2edaa856eddd3723dc181778
Andre Jarboe <andrejarboe@gmail.com>
2023-08-11 21:32:30 -0500

    hello

---
+++ Project/hello.cpp
@@ -0,0 +1,13 @@
+/*
+ * hello.cpp
+ * prints out my name, town
+ * and hobby
+ */
+#include <cstdio>
+
+int main()
+{
+    printf("Hello, name is Andre.\n");
+    printf("Town\n");
+    printf("Hobby\n");
+}
