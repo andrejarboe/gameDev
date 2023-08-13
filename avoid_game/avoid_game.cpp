7058abc15e22895590e8de7a588a67ffbd488a53
Andre Jarboe <andrejarboe@gmail.com>
2023-08-12 22:11:08 -0500

    draw circle

--- avoid_game/avoid_game.cpp
+++ avoid_game/avoid_game.cpp
@@ -1,7 +1,24 @@
 #include "raylib.h"
 
-
 int main()
 {
-    InitWindow(350, 200, "Andre's Game");
+    // window dimensions
+    int width {350};
+    int height {200};
+    InitWindow(width, height, "Andre's Game");
+
+    // circle coordinates
+    int circle_x{175};
+    int circle_y{100};
+
+    while (WindowShouldClose() != true)
+    {
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+
+        DrawCircle(circle_x, circle_y, 25, RED);
+        EndDrawing();
+    }
+    return 0;
 }
